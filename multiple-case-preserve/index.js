b6e54a5ce610666a89551039c66c1bad003eecf5
dannycahyo <dannydwicahyono@gmail.com>
2023-01-03 20:00:47 +0700

    upload multiple case server extension

---
+++ multiple-case-preserve/index.js
@@ -0,0 +1,36 @@
+function howToMoveOnAfterBreakUp() {
+  return "Making Shorts Contents";
+}
+
+const isItOkayToMisuh = true;
+
+let misuhType;
+
+if (isItOkayToMisuh) {
+  misuhType = "Javanese";
+}
+
+console.log(howToMoveOnAfterBreakUp());
+
+// lower case
+const misuh = "cok";
+
+// upper case
+const MISUH = "COK";
+
+// camelCase
+function getMisuhWord() {
+  return "Cok";
+}
+
+// SNAKE_CASE
+const TOTAL_MISUH = 10;
+
+// PascalCase
+function MisuhComponent() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
